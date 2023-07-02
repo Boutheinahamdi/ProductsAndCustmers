be46e072cfc67dde2a95ba919e4a6903046747d8
Boutheinahamdi <bouthayna.hamdi01@gmail.com>
2023-07-02 18:36:17 +0000

    v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import {ProductsComponent} from "./products/products.component";
+import {CustmersComponent} from "./custmers/custmers.component";
+const routes: Routes = [
+  {path: "products",component: ProductsComponent},
+  {path: "custmers",component: CustmersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
